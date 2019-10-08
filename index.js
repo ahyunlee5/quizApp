9164fc55a81284832c8a19a96d5c3a61a28c3707
ahyunlee5 <ahyunlee@umich.edu>
2019-10-08 14:58:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const STORE = [
+  {
+    question : 'How do you figure out how long it will take to double your money?',
+    answer : ['The Rule of 72', 'Double your money and divide by months investing', 'Hard to determine', 'YOLO just wing it'],
+    correctAnswer : 'The rule of 72. By dividing 72 by the annual rate of return you get a rough estiamte',
+  },
+  {
+    question : 'What type of investment strategy gives the highest return?',
+    answer : ['Savings', 'Mutual Funds', 'CD', 'Stocks'],
+    correctAnswer : 'Stock',
+  },
+  {
+    question : 'What is a maturity date?',
+    answer : ['The day retirement fund is released', 'The date when debt is due for payment', 'When bond is due', 'Beginning of your grace period'],
+    correctAnswer : 'The date when debt is due for payment',
+  },
+  {
+    question : 'What is a dividend',
+    answer : ['Your total stock value', 'Fee for stock broker', 'Stock share earnings', 'Value divided by time period to estimate value in a given time'],
+    correctAnswer : 'Stock share earnings',
+  },
+  {
+    question : 'When should you start saving for retirement',
+    answer : ['As soon as you get your first paycheck', 'When you enter college', 'When you get advised by a broker', 'ASAP mang'],
+    correctAnswer : 'ASAP mang',
+  }
+];
