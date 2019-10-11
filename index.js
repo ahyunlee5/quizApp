3fe8daef515df231210aaeaea7d3e767521c7bb0
ahyunlee5 <ahyunlee@umich.edu>
2019-10-10 19:48:12 -0700

    fourth commit

--- index.js
+++ index.js
@@ -27,3 +27,104 @@ const STORE = [
     correctAnswer : 'ASAP mang',
   }
 ];
+//global variables
+let score = 0;
+let questionNumber = 0;
+
+function startQuiz() {
+  //start quiz after pressing button
+  $('#js-start-button').on('click', function() {
+    $('.startQuiz').remove();
+    $('.questionAnswerForm').css('display', 'block');
+    $('.questionNumber').text(1);
+    renderQuestion();
+    console.log('quiz started');
+  });
+}
+
+function generateQuestion() {
+  //go through STORE to pull up questions
+  //needs to update html to include form for question
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form class='form'>
+    <fieldset class='answer-choices'>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answer[0]}" name="answer" required>
+    <span>${STORE[questionNumber].answer[0]}</span>
+    </label>
+    <br>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answer[1]}" name="answer" required>
+    <span>${STORE[questionNumber].answer[1]}</span>
+    </label>
+    <br>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answer[2]}" name="answer" required>
+    <span>${STORE[questionNumber].answer[2]}</span>
+    </label>
+    <br>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answer[3]}" name="answer" required>
+    <span>${STORE[questionNumber].answer[3]}</span>
+    </label>
+    </fieldset>
+    <br>
+    <button type="submit" class="submitButton">Submit</button>
+    </form>
+    </div>`;
+  } else {
+    return final();
+  }
+}
+
+function renderQuestion() {
+  $('.questionAnswerForm').html(generateQuestion());
+  console.log('rendering');
+}
+
+function scoreUpdate() {
+  //through each question updates the score if question is answered correctly
+  score++;
+}
+
+function questionNumberUpdate() {
+  //increases question 
+  questionNumber++;
+  $('.questionNumber').text(questionNumber + 1);
+  console.log('updated question number');
+}
+
+function checkAnswer() {
+  //checks against correctAnswer to see if it's compatiable
+}
+
+function rightAnswer() {
+  //when answer is answered correclty
+  score ++;
+}
+
+function wrongAnswer() {
+  //when answer is incorrect
+  score;
+}
+
+function nextQuestion() {
+  //next question
+}
+
+function final() {
+  //displays final score\
+}
+
+function allFunctions() {
+  startQuiz();
+  scoreUpdate();
+  checkAnswer();
+  rightAnswer();
+  wrongAnswer();
+  nextQuestion();
+}
+
+$(allFunctions);
